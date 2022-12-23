5b93acb83c29318e468cc462e437f1e8c2d7b8e5
Jhon Bellido <jhonbellidovargas@gmail.com>
2022-12-23 10:22:51 -0500

    tarea 17

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const userRoutes = require("./routes/user");
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+//Middleware
+app.use(express.json());
+app.use('/api', userRoutes);
+//Routes
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+//MongoDB Connection
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+app.listen(port, () => {
+  console.log("Server is running in http://localhost:" + port);
+});
